e9c4ec689645cc93de22340798662d6492c83c5c
Mubin21 <mmacadadaya2016@gmail.com>
2023-01-21 08:12:35 +0800

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="hhh lpR fff">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          ParkFinder
+        </q-toolbar-title>
+        <q-btn>
+          Login
+        </q-btn>
+        <q-btn>
+          Signup
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="text-center">
+            <p>A Car Parking Finder App</p>
+          </div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <p>Image</p>
+    <p>Description</p>
+    <button>Get Started</button>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
